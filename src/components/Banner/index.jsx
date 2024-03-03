b536553c5c7f084025563958ac8a275c1d45e355
Gabriel  Alves <68339733+gabbezeira@users.noreply.github.com>
2024-03-03 02:08:18 -0300

    fix header mobile

--- src/components/Banner/index.jsx
+++ src/components/Banner/index.jsx
@@ -3,11 +3,6 @@ import BannerImg from "../../assets/images/banner.svg"
 
 export function Banner() {
   return (
-    <BannerStyle>
-        <img className="banner"
-          src={BannerImg}
-          alt="Banner contendo uma imagem promocional do game GTA 6"
-        />
-    </BannerStyle>
+    <BannerStyle style={{backgroundImage: `url(${BannerImg})`}}/>
   )
 }
--- src/components/Banner/styles.jsx
+++ src/components/Banner/styles.jsx
@@ -2,11 +2,17 @@ import styled from "styled-components"
 import "../../styles/index.css"
 
 export const BannerStyle = styled.div`
-  .banner {
-    width: 100%;
-    height: auto;
-    margin: 5rem 0rem;
-    pointer-events: none;
-    user-select: none;
+  width: 100%;
+  height: 40.625rem;
+  margin: 5rem 0rem;
+  pointer-events: none;
+  user-select: none;
+
+  background-position: center;
+
+  @media(max-width: 640px){
+    height: 31.25rem;
+    background-position: center;
+    background-size: cover;
   }
 `
