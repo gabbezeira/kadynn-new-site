5075ff14c37a73a25d90f98be2fa142b2c112a30
gabbezeira <gabrielalves.code@gmail.com>
2025-04-15 15:34:33 -0300

    ajustes

--- src/components/Banner/index.jsx
+++ src/components/Banner/index.jsx
@@ -1,5 +1,5 @@
 import { Container } from './styles'
-import BannerImg from '@assets/images/logo.jpg'
+import BannerImg from '@assets/images/ssss.jpg'
 
 export function Banner() {
   return <Container style={{ backgroundImage: `url(${BannerImg})` }} />
