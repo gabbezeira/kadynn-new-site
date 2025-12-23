4821feb0cbb28e1688e488c03bd68981abb34dfc
gabbezeira <gabrielalves.code@gmail.com>
2025-12-23 10:49:48 -0300

    feat: december snow

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,36 @@
-import { Body, PageWrapper } from './styles/styles'
-import { Outlet } from 'react-router-dom'
-
-import { Menu, Banner, Footer } from '@components'
-import { Analytics } from '@vercel/analytics/react'
+import { Banner, Footer, Menu } from "@components";
+import { Analytics } from "@vercel/analytics/react";
+import { Outlet } from "react-router-dom";
+import Snowfall from "react-snowfall";
+import { Body, PageWrapper } from "./styles/styles";
 
 export default function App() {
-  return (
-    <>
-      <PageWrapper>
-        <Menu />
-        <Banner />
-        <Body>
-          <Outlet />
-        </Body>
-        <Analytics />
-        <Footer />
-      </PageWrapper>
-    </>
-  )
+	const isDecember = new Date().getMonth() === 11;
+
+	return (
+		<PageWrapper>
+			{isDecember && (
+				<Snowfall
+					snowflakeCount={120}
+					style={{
+						position: "fixed",
+						width: "100vw",
+						height: "100vh",
+						pointerEvents: "none",
+						zIndex: 9999,
+					}}
+				/>
+			)}
+
+			<Menu />
+			<Banner />
+
+			<Body>
+				<Outlet />
+			</Body>
+
+			<Analytics />
+			<Footer />
+		</PageWrapper>
+	);
 }
